f93be5ac42830f9dc6988126cf4c82a82b49b90b
R. Taylor Raborn <Randolph.Raborn@st.dhs.gov>
2026-04-10 14:59:54 -0400

    Added re-norm for times when wta is on but calc. cov. is off.

--- src/yacht/run_YACHT.py
+++ src/yacht/run_YACHT.py
@@ -277,7 +277,7 @@ def main(args):
             temp_manifest = manifest_list[0].copy()
             if not show_all:
                 temp_manifest = temp_manifest[temp_manifest["in_sample_est"] == True]
-		# Adding re-normalization here also
+		        # Adding re-normalization here also
                 total_abundance = temp_manifest['rel_abund'].sum()
                 if total_abundance > 0:
                     temp_manifest.loc[:, 'rel_abund'] = temp_manifest['rel_abund'] / total_abundance
@@ -306,6 +306,9 @@ def main(args):
             for min_coverage, temp_manifest in zip(min_coverage_list, manifest_list):
                 if not show_all:
                     temp_manifest = temp_manifest[temp_manifest["in_sample_est"] == True]
+                    total_abundance = temp_manifest['rel_abund'].sum()
+                    if total_abundance > 0:
+                        temp_manifest.loc[:, 'rel_abund'] = temp_manifest['rel_abund'] / total_abundance
                 temp_manifest.to_excel(
                     writer, sheet_name=f"min_coverage{min_coverage}", index=False
                 )
